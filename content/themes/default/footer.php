4330d545f3a0b6f2dc4368d37b636329a71c6a72
Saeed Vaziry <sa.vaziry@gmail.com>
2019-07-08 00:23:02 +0430

    cleanup for v1

--- content/themes/default/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('BASE_PATH')) {
-    echo "<h1>404 Not Found</h1>";
-    exit;
-}
-?>
-</body>
-</html>
--- includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : "";
-
-if (!$userId) {
-    header('location: ' . url('/login.php'));
-    exit;
-}
